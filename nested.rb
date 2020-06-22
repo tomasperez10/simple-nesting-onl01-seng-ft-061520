4d04efc7a8d7802130f170e9210997bccf46ab07
Tomas Perez <tomasitopg6@gmail.com>
2020-06-22 23:01:35 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  
+  programmer_hash[:alan_kay][:known_for]
+  
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -77,6 +83,10 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
+        },
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
         }
      }
 
@@ -102,6 +112,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
      
 end
 
@@ -121,5 +133,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 end
